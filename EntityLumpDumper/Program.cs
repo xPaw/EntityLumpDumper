4a5eab04ce43221c7756f25e0938c6930ccc4961
Pavel Djundik <xPaw@users.noreply.github.com>
2020-11-08 13:39:10 +0200

    Basic bsp entity lump dumper

---
+++ EntityLumpDumper/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using LibBSP;
+
+namespace EntityLumpDumper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.Error.WriteLine($"Provide path to the bsp file");
+                Environment.Exit(1);
+            }
+
+            var map = args[0];
+
+            if (!File.Exists(map))
+            {
+                Console.Error.WriteLine($"Map does not exist: {map}");
+                Environment.Exit(1);
+            }
+
+            Console.WriteLine($"Reading {map}");
+
+            var bsp = new BSP(map);
+            var bytes = bsp.reader.ReadLump(bsp.reader.GetLumpInfo(Entity.GetIndexForLump(bsp.version), bsp.version));
+
+            if (bytes.Length > 0 && bytes[^1] == 0x00)
+            {
+                bytes[^1] = 0x0A;
+            }
+            
+            var dump = Path.ChangeExtension(map, "lmp");
+            File.WriteAllBytes(dump, bytes);
+
+            Console.WriteLine($"Written {bytes.Length} bytes to {Path.GetFileName(dump)}");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+Dump entity lump from BSP maps (CS:GO, TF2, etc..)
+
+Get LibBSP here: https://github.com/wfowler1/LibBSP
+Get 7zip LZMA here: https://www.7-zip.org/sdk.html
